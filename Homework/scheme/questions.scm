7b4d2d40955fac3027bc12fc4c5cbde8a2de54e3
ioyoi <i@ioyoi.me>
2025-03-03 21:48:16 +0800

    12.2 Scheme Problem except for EC

--- Homework/scheme/questions.scm
+++ Homework/scheme/questions.scm
@@ -7,8 +7,10 @@
 ;; Returns a list of two-element lists
 (define (enumerate s)
   ; BEGIN PROBLEM 15
-  'replace-this-line
-  )
+  (begin (define (enumerate_helper n s)
+    (if (null? s) () (cons (list n (car s)) (enumerate_helper (+ n 1) (cdr s)))))
+    (enumerate_helper 0 s)
+  ))
   ; END PROBLEM 15
 
 ;; Problem 16
@@ -17,8 +19,11 @@
 ;; the merged lists.
 (define (merge ordered? s1 s2)
   ; BEGIN PROBLEM 16
-  'replace-this-line
-  )
+  (cond
+    ((null? s1) s2)
+    ((null? s2) s1)
+    ((ordered? (car s1) (car s2)) (cons (car s1) (merge ordered? (cdr s1) s2)))
+    (else (cons (car s2) (merge ordered? (cdr s2) s1)))))
   ; END PROBLEM 16
 
 ;; Optional Problem 2
