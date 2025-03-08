6aebe6881ea5d5bd001d9cb52a90abad82355ca5
ioyoi <i@ioyoi.me>
2025-03-08 22:48:00 +0800

    Archieve.

--- Lecture/09-Counting-Partitions.py
+++
@@ -1,6 +0,0 @@
-def count_partitions(n, k):
-    if n < k:
-        return 0
-    if k == 1:
-        return n
-    return count_partitions(n-k, k) + count_partitions(n, k-1)
---
+++ Website/README.md
@@ -0,0 +1,2 @@
+# CS61A Fall 2024
+Archived [CS61A](https://cs61a.org/) Fall 2024
